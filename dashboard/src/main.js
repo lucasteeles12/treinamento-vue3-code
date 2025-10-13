cad112c01018a43cc743c0f0e6b491267205db0d
lucasteeles12 <lucaslol2004@gmail.com>
2025-10-13 11:57:49 -0300

    ..

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,14 +1,12 @@
+import './assets/css/main.css'
+
 import { createApp } from 'vue'
-import Toast, { POSITION } from 'vue-toastification'
 import App from './App.vue'
 import router from './router'
 
 import 'animate.css'
-import '@/assets/css/tailwind.css'
-import '@/assets/css/fonts.css'
-import 'vue-toastification/dist/index.css'
+import '../src/assets/css/fonts.css'
 
 const app = createApp(App)
 app.use(router)
-app.use(Toast, { position: POSITION.BOTTOM_RIGHT })
 app.mount('#app')
--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -4,37 +4,35 @@ const Home = () => import('../views/Home/index.vue')
 const Feedbacks = () => import('../views/Feedbacks/index.vue')
 const Credencials = () => import('../views/Credencials/index.vue')
 
-export const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/feedbacks',
-    name: 'Feedbacks',
-    component: Feedbacks,
-    meta: {
-      hasAuth: true
-    }
-  },
-  {
-    path: '/credencials',
-    name: 'Credencials',
-    component: Credencials,
-    meta: {
-      hasAuth: true
-    }
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: { name: 'Home' }
-  }
-]
-
 const router = createRouter({
-  history: createWebHistory('/'),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/feedbacks',
+      name: 'Feedbacks',
+      component: Feedbacks,
+      meta:{
+        hasAuth: true
+      }
+    },
+    {
+      path: '/credencials',
+      name: 'Credencials',
+      component: Credencials,
+      meta:{
+        hasAuth: true
+      }
+    },
+    {
+      path:'/:pathMatch(.*)*',
+      redirect: {name: 'Home'}
+    }
+  ],
 })
 
 export default router
